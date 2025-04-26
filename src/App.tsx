2d032030f0ae4fb40a12ec084dbd605735b8f252
Pedro Moraes <pmoraes@esapiens.com.br>
2025-04-26 16:01:50 -0300

    chore: include comunication with backend

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,27 @@
 import { useQuery, gql } from '@apollo/client'
 import LoadingPage from './components/Loading/Loading'
 
-interface ILocation {
-  id: string
-  name: string
-  description: string
-  photo: string
+interface IOrders {
+  instrument: string
 }
 
-const GET_LOCATIONS = gql`
-  query GetLocations {
-    locations {
-      id
-      name
-      description
-      photo
+const GET_ORDERS = gql`
+  query GetOrders {
+    orders {
+      instrument
     }
   }
 `
 
 function DisplayLocations() {
-  const { loading, error, data } = useQuery(GET_LOCATIONS)
+  const { loading, error, data } = useQuery(GET_ORDERS)
 
   if (loading) return <LoadingPage />
   if (error) return <p>Error : {error.message}</p>
 
-  return data.locations.map(({ id, name, description, photo }: ILocation) => (
-    <div key={id}>
-      <h3>{name}</h3>
-      <img width="400" height="250" alt="location-reference" src={`${photo}`} />
-      <br />
-      <b>About this location:</b>
-      <p>{description}</p>
-      <br />
+  return data.orders.map(({ instrument }: IOrders) => (
+    <div key={instrument}>
+      <h3 className="bg-red-400">{instrument}</h3>
     </div>
   ))
 }

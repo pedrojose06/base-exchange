3f547f9c13e209e2c250b82dc3393f6408b20834
Pedro Moraes <pmoraes@esapiens.com.br>
2025-04-27 21:11:40 -0300

    feat: add callback to function to change status

--- src/hooks/useOrderChangeStatus.ts
+++ src/hooks/useOrderChangeStatus.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react'
 import { OrderStatus } from '@/constants/order'
 import { SEND_ORDER_CANCEL } from '@/graphql/mutations/sendOrderCancel'
 import { useMutation } from '@apollo/client'
@@ -6,16 +7,19 @@ const useOrderChangeStatus = () => {
   const [changeOrderStatusMutation, { data, loading, error }] =
     useMutation(SEND_ORDER_CANCEL)
 
-  const changeOrderStatus = async (orderId: string, status: OrderStatus) => {
-    try {
-      const response = await changeOrderStatusMutation({
-        variables: { id: orderId, status: status },
-      })
-      return response.data.updateOrderStatus
-    } catch (err) {
-      throw err
-    }
-  }
+  const changeOrderStatus = useCallback(
+    async (orderId: string, status: OrderStatus) => {
+      try {
+        const response = await changeOrderStatusMutation({
+          variables: { id: orderId, status: status },
+        })
+        return response.data.updateOrderStatus
+      } catch (err) {
+        throw err
+      }
+    },
+    [changeOrderStatusMutation] // Dependency array
+  )
 
   return {
     changeOrderStatus,

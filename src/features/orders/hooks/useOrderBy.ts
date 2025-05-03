4f8d1032ab634713d658839c99213107801f6520
Pedro Moraes <pmoraes@esapiens.com.br>
2025-05-03 09:14:34 -0300

    fix: fix filter that was not working

--- src/features/orders/hooks/useOrderBy.ts
+++ src/features/orders/hooks/useOrderBy.ts
@@ -22,20 +22,23 @@ export const useOrderBy = () => {
   })
 
   const executeFilters = async (newFilters: IOrderFilter) => {
-    setFilters(newFilters)
     if (
-      Object.keys(filters).length === 0 ||
-      (filters.createdAt === '' &&
-        filters.id === '' &&
-        filters.status === '' &&
-        filters.side === 0 &&
-        filters.instrument === '')
+      Object.keys(newFilters).length === 0 ||
+      (newFilters.createdAt === '' &&
+        newFilters.id === '' &&
+        newFilters.status === '' &&
+        newFilters.side === 0 &&
+        newFilters.instrument === '')
     )
       return
 
+    setFilters(newFilters)
+
     try {
-      const { data } = await refetchByFilters({ filters: newFilters })
-      setOrderList(data?.ordersByFilter.orders || [])
+      const newOrderList = await refetchByFilters({ filters: newFilters })
+
+      setOrderList(newOrderList.data.ordersByFilter.orders)
+
       setActualPage(1)
     } catch (err) {
       console.error('Error fetching data:', err)

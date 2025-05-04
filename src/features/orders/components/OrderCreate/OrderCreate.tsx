d1d2ed1ef1d63d5a9ca078a8118a0fb1b4b7cf22
Pedro Moraes <pmoraes@esapiens.com.br>
2025-05-04 14:18:55 -0300

    fix: separate intrument in a constant file

--- src/features/orders/components/OrderCreate/OrderCreate.tsx
+++ src/features/orders/components/OrderCreate/OrderCreate.tsx
@@ -21,24 +21,12 @@ import {
 } from '@/components/ui/select'
 import useOrderCreate from '@/features/orders/hooks/useOrderCreate'
 import DialogModal from '../../../../components/DialogModal/DialogModal'
+import { instruments } from '../../constants/intruments'
 
 interface IOrderCreate {
   open: boolean
   onClose: () => void
 }
-
-const instruments = [
-  { id: 1, name: 'PETR4', price: 5.34 },
-  { id: 2, name: 'VALE3', price: 67.89 },
-  { id: 3, name: 'ITUB4', price: 28.45 },
-  { id: 4, name: 'BBAS3', price: 36.12 },
-  { id: 5, name: 'ABEV3', price: 14.67 },
-  { id: 6, name: 'MGLU3', price: 3.21 },
-  { id: 7, name: 'LREN3', price: 25.78 },
-  { id: 8, name: 'WEGE3', price: 40.56 },
-  { id: 9, name: 'B3SA3', price: 12.34 },
-  { id: 10, name: 'BRFS3', price: 8.9 },
-]
 
 const orderSchema = z.object({
   instrument: z.string().nonempty('Selecione um instrumento financeiro.'),
